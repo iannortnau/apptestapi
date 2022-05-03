df8680cc9717a0b1d858fa7bec125f739f46ffe6
iannortnau <ortnau.iann@gmail.com>
2022-05-03 07:09:24 -0300

    rota para gerar dados aleatórios para o app de descontos

---
+++ pages/api/discounts/[amount].js
@@ -0,0 +1,29 @@
+import { faker } from '@faker-js/faker';
+
+export default function handler(req, res) {
+    const { amount } = req.query||10;
+
+    switch (req.method) {
+        case 'GET':
+            return getDiscounts();
+        default:
+            return res.status(405).end(`Method ${req.method} Not Allowed`)
+    }
+
+    async function getDiscounts(){
+        const data = {
+            discounts: []
+        }
+        for (let i = 0; i < amount; i++) {
+            const discount = {
+                imageLink: faker.image.avatar(),
+                title: faker.commerce.productName(),
+                subTitle:faker.commerce.productDescription(),
+                qrCode:faker.image.cats()
+            }
+            data.discounts.push(discount);
+        }
+
+        res.status(200).json(data);
+    }
+}
